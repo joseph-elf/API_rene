57c4a51abf20839fc786f239321cec5c67afce5d
Joseph <josephelforzli@gmail.com>
2026-02-27 19:30:03 +0100

    test

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ sys.path.append(os.path.abspath(".."))
 from package import *
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    #allow_origins=["http://localhost:3000","http://10.186.103.211:3000"],  # your Next.js app
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "API is working"}
